e5b00db548a6c5e1ff6be47c41ec069fae6880a0
lpc84 <76259857+lpc84@users.noreply.github.com>
2021-06-30 18:22:49 +0100

    log env data

--- src/_data/events.js
+++ src/_data/events.js
@@ -2,6 +2,11 @@ const axios = require('axios');
 
 
 module.exports = async function run(){
+    console.log(`------------------------------------`)
+    console.log(process.env.STRAPI_URL)
+    console.log(process.env.STRAPI_API_KEY)
+    console.log(process.env.STRAPI_API_PASS)
+    console.log(`------------------------------------`)
     const { data } = await axios.post(`${process.env.STRAPI_URL}/auth/local`, {
       identifier: process.env.STRAPI_API_KEY,
       password: process.env.STRAPI_API_PASS,
