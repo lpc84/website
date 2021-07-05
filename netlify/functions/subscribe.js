99c88868306037ed251cbab13c04edfa6c651acf
lpc84 <76259857+lpc84@users.noreply.github.com>
2021-07-05 22:03:27 +0100

    fix subscribe

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -1,3 +1,3 @@
-module.exports = async function return200JSON(event, context) {
+exports.handler = async (event, context) => {
   return { statusCode: 200, body: JSON.stringify({ message: "Hello World" }) };
 };
